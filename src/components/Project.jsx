82351981f80a6ac075f21fce9a3827d5afd19ce9
yarphen <sheremet.mykhaylo@gmail.com>
2019-02-21 18:52:33 +0200

    Extend eslint config, fix mistakes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 class Project extends PureComponent {
   render() {
@@ -11,9 +10,7 @@ class Project extends PureComponent {
       <div style={{ display: 'flex', flexDirection: 'column', margin: '50px' }}>
         <img src={img} style={{ width: '400px' }} />
         {title}
-        <a href={fbLink}>
-              Facebook
-        </a>
+        <a href={fbLink}>Facebook</a>
         {available ? <div> Open</div> : <div> Close</div>}
       </div>
     );
@@ -22,7 +19,7 @@ class Project extends PureComponent {
 Project.propTypes = {
   img: PropTypes.string.isRequired,
   fbLink: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
   available: PropTypes.bool.isRequired,
 };
 // const mapDispatchToProps = dispatch => ({
