751f397167942de825b97b66b987df4945d8ce18
yarphen <sheremet.mykhaylo@gmail.com>
2019-03-01 03:20:36 +0200

    Add TODO

--- src/selectors/members.js
+++ src/selectors/members.js
@@ -1,3 +1,6 @@
+// TODO use reselect as the only way to compose selectors
+// and make all selectors O(1) in time
+
 export const selectorMembers = state => state.members.result;
 export const selectorProfileById = (state, id) => selectorMembers(state) && selectorMembers(state).find(x => x.id === id);
 export const selectorPatron = (state, childId) => {
