e0cd99dbd653dad211a19ef4af72864593662fdd
gitbrik <45262648+gitbrik@users.noreply.github.com>
2019-03-23 00:58:44 +0200

    no bootstrap demo (#19)

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -20,127 +20,125 @@ class SearchForm extends PureComponent {
   render() {
     const { role, faculty: selectedFaculty } = this.props;
     const { admin } = role || {};
-    console.log(selectedFaculty);
     const { specialities = [] } = FACULTIES.find(({ value }) => value === selectedFaculty) || {};
     return (
-      <form>
-        <div className="row d-flex justify-content-center text-center">
-          <div className="col-10 border-top-blue ">
-            <div className="row mt-3 mb-2">
-              <div className="col-2 accounts-admin_control_search-icon-box">
-                <i className="fa fa-search" />
-              </div>
-              <div className="col-9">
-                <Field component="input" type="text" name="quicksearch"
-                  className="form-control accounts-admin_control_search-input"
-                  placeholder="Пошук" />
-              </div>
+      <form className="contacts-search-form">
+        <div className="contacts-search-wrapper">
+          <div className="contacts-search-wrapper_search-line-box border-top-blue ">
+            <div className=" accounts-admin_control_search-icon-box">
+              <i className="fa fa-search" />
+            </div>
+            <div>
+              <Field component="input" type="text" name="quicksearch"
+                className="accounts-admin_control_search-input"
+                placeholder="Пошук" />
             </div>
           </div>
-          <div className="col-10 border-top-blue pt-2 pb-2 mb-2">
-            <button className="btn btn-block font-weight-bold accounts-admin_control_classes-btn" type="button">
-              НІЧИМ НЕ ЗАЙНЯТІ
+          <div className="contacts-search-wrapper_classes border-top-blue">
+            <button className="contacts-search-wrapper_classes_btn"
+              type="button">
+                            НІЧИМ НЕ ЗАЙНЯТІ
             </button>
-            <button className="btn btn-block font-weight-bold accounts-admin_control_classes-btn" type="button">
-              НЕОПАТРОНЕНІ
+            <button className="contacts-search-wrapper_classes_btn"
+              type="button">
+                            НЕОПАТРОНЕНІ
             </button>
-            <button className="btn btn-block font-weight-bold accounts-admin_control_classes-btn" type="button">
-              МОЇ МАЛЮКИ
+            <button className="contacts-search-wrapper_classes_btn"
+              type="button">
+                            МОЇ МАЛЮКИ
             </button>
-            <button className="btn btn-block font-weight-bold accounts-admin_control_classes-btn" type="button">
-              ПОШАНОВАНІ
+            <button className="contacts-search-wrapper_classes_btn"
+              type="button">
+                            ПОШАНОВАНІ
             </button>
-            <button className="btn btn-block font-weight-bold accounts-admin_control_classes-btn" type="button">
-              УПРАВА
+            <button className="contacts-search-wrapper_classes_btn"
+              type="button">
+                            УПРАВА
             </button>
             {admin && (
-              <button className="btn btn-block font-weight-bold accounts-admin_control_classes-btn" type="button">
-                ЗАПИТИ НА ПІДТВЕРДЖЕННЯ
-              </button>
+            <button className="contacts-search-wrapper_classes_btn"
+              type="button">
+                                ЗАПИТИ НА ПІДТВЕРДЖЕННЯ
+            </button>
             )}
           </div>
-
-          <h4 className="col-12 text-mid-blue">ФІЛЬТРИ</h4>
-          <div className="col-12">
-            <Field component="select" name="status" className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg  mt-1 mb-1 p-0 bg-white">
+          <div className="contacts-search-wrapper_filters">
+            <h4 className="text-mid-blue">ФІЛЬТРИ</h4>
+            <Field component="select" name="status"
+              className="text-mid-blue contact-search_select">
               <option default value="">Статус в СБ</option>
               {SB_STATUS.map(({ value, label }) => <option key={value} value={value}>{label}</option>)}
             </Field>
-          </div>
-          <div className="col-12">
+
             <select
-              className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg mt-1 mb-1 p-0 bg-white">
+              className="text-mid-blue contact-search_select">
               <option>Патрон</option>
             </select>
-          </div>
-          <div className="col-sm-12 col-md-6">
-            <Field component="select" name="avatar" className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg mt-1 mb-1 p-0 bg-white">
-              <option default value="">Фото</option>
-              <option default value="true">Є фото</option>
-              <option default value="false">Нема фото</option>
-            </Field>
-          </div>
-          <div className="col-sm-12 col-md-6">
-            <Field component={DatePickerField} name="birthday" className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg mt-1 mb-1 p-0 bg-white" placeholder="Дата народження" />
-          </div>
-          <h5 className="mt-3 col-12 text-mid-blue">ОСВІТА В НАУКМА</h5>
-          <div className="col-sm-12 col-md-6">
-            <Field component="select" name="faculty" className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg  mt-1 mb-1 p-0 bg-white">
-              <option default value="">Факультет</option>
-              {FACULTIES.map(({ name, value }) => <option key={value} value={value}>{name}</option>)}
-            </Field>
-          </div>
-          <div className="col-sm-12 col-md-6">
-            <Field component="select" name="speciality" className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg  mt-1 mb-1 p-0 bg-white">
-              <option default value="">Спеціальність</option>
-              {specialities.map(({ name, value }) => <option key={value} value={value}>{name}</option>)}
-            </Field>
-          </div>
-          <div className="col-sm-12 col-md-6">
-            <Field component="select" name="program" className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg  mt-1 mb-1 p-0 bg-white">
-              <option default value="">Програма</option>
-              {PROGRAMS.map(({ name, value }) => <option key={value} value={value}>{name}</option>)}
-            </Field>
-          </div>
-          <div className="col-sm-12 col-md-6">
-            <Field component="select" name="entranceYear" className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg  mt-1 mb-1 p-0 bg-white">
-              <option default value="">Рік вступу</option>
-              {POSSIBLE_YEARS.map(item => <option item={item} value={item}>{item}</option>)}
-            </Field>
-          </div>
+            <div className="contacts-search-wrapper_filters_two-in-row-box">
+              <Field component="select" name="avatar"
+                className="text-mid-blue contact-search_select small">
+                <option default value="">Фото</option>
+                <option default value="true">Є фото</option>
+                <option default value="false">Нема фото</option>
+              </Field>
+              <Field component={DatePickerField} name="birthday"
+                className="text-mid-blue contact-search_select"
+                placeholder="Дата народження" />
+            </div>
+            <h5 className="text-mid-blue">ОСВІТА В НАУКМА</h5>
+            <div className="contacts-search-wrapper_filters_two-in-row-box">
+              <Field component="select" name="faculty"
+                className="text-mid-blue contact-search_select small">
+                <option default value="">Факультет</option>
+                {FACULTIES.map(({ name, value }) => <option key={value} value={value}>{name}</option>)}
+              </Field>
 
-          <h5 className="text-mid-blue mt-3 col-12 text-mid-blue">КОНТАКТИ</h5>
-          <div className="col-12">
-            <Field component="input" name="email" className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg mt-1 mb-1 p-0 bg-white" placeholder="E-mail" />
-          </div>
-          <div className="col-12">
-            <Field component="input" name="phone" className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg mt-1 mb-1 p-0 bg-white" placeholder="Телефон" />
-          </div>
-          <div className="col-12">
-            <Field component="input" name="profile" className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg mt-1 mb-1 p-0 bg-white" placeholder="Профіль" />
-          </div>
-          <h5 className="text-mid-blue mt-3 col-12">ДІЯЛЬНІСТЬ В СБ</h5>
-          <div className="col-sm-12 col-md-6">
+              <Field component="select" name="speciality"
+                className="text-mid-blue contact-search_select small">
+                <option default value="">Спеціальність</option>
+                {specialities.map(({ name, value }) => <option key={value} value={value}>{name}</option>)}
+              </Field>
+            </div>
+            <div className="contacts-search-wrapper_filters_two-in-row-box">
+              <Field component="select" name="program"
+                className="text-mid-blue contact-search_select small">
+                <option default value="">Програма</option>
+                {PROGRAMS.map(({ name, value }) => <option key={value} value={value}>{name}</option>)}
+              </Field>
+              <Field component="select" name="entranceYear"
+                className="text-mid-blue contact-search_select small">
+                <option default value="">Рік вступу</option>
+                {POSSIBLE_YEARS.map(item => <option item={item} value={item}>{item}</option>)}
+              </Field>
+            </div>
+
+            <h5 className="text-mid-blue">КОНТАКТИ</h5>
+            <Field component="input" name="email"
+              className="text-mid-blue contact-search_select"
+              placeholder="E-mail" />
+            <Field component="input" name="phone"
+              className="text-mid-blue contact-search_select"
+              placeholder="Телефон" />
+            <Field component="input" name="profile"
+              className="text-mid-blue contact-search_select"
+              placeholder="Профіль" />
+            <h5 className="text-mid-blue">ДІЯЛЬНІСТЬ В СБ</h5>
+            <div className="contacts-search-wrapper_filters_two-in-row-box">
+              <select
+                className="text-mid-blue contact-search_select small">
+                <option>01.01.1995</option>
+              </select>
+              <select
+                className="text-mid-blue contact-search_select small">
+                <option>Фото є</option>
+              </select>
+            </div>
             <select
-              className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg  mt-1 mb-1 p-0 bg-white">
-              <option>01.01.1995</option>
-            </select>
-          </div>
-          <div className="col-sm-12 col-md-6">
-            <select
-              className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg mt-1 mb-1 p-0 bg-white">
-              <option>Фото є</option>
-            </select>
-          </div>
-          <div className="col-12">
-            <select
-              className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg mt-1 mb-1 p-0 bg-white">
+              className="text-mid-blue contact-search_select">
               <option>Статус в СБ</option>
             </select>
-          </div>
-          <div className="col-12">
             <select
-              className="text-mid-blue accounts-admin_control_filter-select form-control form-control-lg  mt-1 mb-1 p-0 bg-white">
+              className="text-mid-blue contact-search_select">
               <option>Статус в СБ</option>
             </select>
           </div>
